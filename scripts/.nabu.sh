6786ffeda10ef210881a914cd6854b00fd68cb05
flizze <alexandre.bel@ynov.com>
2025-11-14 02:33:44 +0100

    updated nabu script

--- scripts/.nabu.sh
+++ scripts/.nabu.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
 CLIENT_SESSION="Nabu"
-CLIENT_EXISTS=$(tmux list-sessions | grep $CLIENT_SESSION)
+ROOT_DIR="$HOME/Documents/Nabu/WEBSITE"
 
-if [ "$CLIENT_EXISTS" = "" ]; then
-    tmux new-session -d -s $CLIENT_SESSION
+if ! tmux has-session -t "$CLIENT_SESSION" 2>/dev/null; then
+    tmux new-session -d -s "$CLIENT_SESSION" -c "$ROOT_DIR" -n "vim"
+    tmux send-keys -t "$CLIENT_SESSION:vim" "vim src" C-m
 
-    tmux new-window -t $CLIENT_SESSION:1 -n "vim"
-    tmux send-keys -t $CLIENT_SESSION:1 "cd ~/Documents/Nabu/WEBSITE/src && vim ." C-m
+    tmux new-window -t "$CLIENT_SESSION" -n "serve" -c "$ROOT_DIR"
+    tmux send-keys -t "$CLIENT_SESSION:serve" "bun run dev" C-m
 
-    tmux new-window -t $CLIENT_SESSION:2 -n "serve"
-    tmux send-keys -t $CLIENT_SESSION:2 "cd ~/Documents/Nabu/WEBSITE/ && bun run dev" C-m
+    tmux new-window -t "$CLIENT_SESSION" -n "tsc" -c "$ROOT_DIR"
+    tmux send-keys -t "$CLIENT_SESSION:tsc" "tsc --noEmit --watch" C-m
 
-    tmux new-window -t $CLIENT_SESSION:3 -n "tsc"
-    tmux send-keys -t $CLIENT_SESSION:3 "cd ~/Documents/Nabu/WEBSITE/ && tsc --noEmit --watch" C-m
-
-    tmux select-window -t $CLIENT_SESSION:1
+    tmux select-window -t "$CLIENT_SESSION:vim"
 fi
 
-tmux attach-session -t $CLIENT_SESSION
+tmux attach-session -t "$CLIENT_SESSION"
