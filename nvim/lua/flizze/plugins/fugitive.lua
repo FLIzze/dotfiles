5acc9e883b86fdc08f18347944c26872612dc0a2
flizze <alexandre.bel@ynov.com>
2026-05-27 09:56:59 +0200

    updated fugitive again

--- nvim/lua/flizze/plugins/fugitive.lua
+++ nvim/lua/flizze/plugins/fugitive.lua
@@ -5,10 +5,8 @@ return {
         local keymap = vim.keymap.set
 
         keymap("n", "<leader>gs", vim.cmd.Git, { desc = "Git status" })
-        keymap("n", "<leader>gc", ":Git commit<cr>", { desc = "Git commit" })
-        keymap("n", "<leader>gP", ":Git pull<cr>", { desc = "Git pull" })
         keymap("n", "<leader>gb", ":Git blame<cr>", { desc = "Git blame" })
-        keymap("n", "<leader>gd", ":Gdiffsplit!<cr>", { desc = "Git diff (3-way)" })
+        keymap("n", "<leader>gd", ":Gvdiffsplit!<cr>", { desc = "Git diff (3-way)" })
         keymap("n", "<leader>gh", ":diffget //2<CR>", { desc = "Get LEFT change" })
         keymap("n", "<leader>gl", ":diffget //3<CR>", { desc = "Get RIGHT change" })
     end,
